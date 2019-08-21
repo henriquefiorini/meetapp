f7bf4b8dc8f9b699e9f6f1706e52978cf6a46784
Henrique Fiorini <henriquehfr2@gmail.com>
2019-08-21 15:04:20 -0400

    Authentication and User update method

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,15 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.post('/users', UserController.create);
+routes.put('/users', authMiddleware, UserController.update);
 
 export default routes;
